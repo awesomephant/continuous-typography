36e0cfb0bc6178d9d7401e1f37c123badeb8c573
Max Kohler <max@koehler-kn.de>
2021-02-01 20:48:30 +0000

    Updates

---
+++ precache-manifest.018d0bb39b6de3c8e83bd329e8334721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278384a3ba25e4cb3f6187fa1198a7c0",
+    "url": "/continuous-typography/index.html"
+  },
+  {
+    "revision": "39b87a2580a5e1c8536b",
+    "url": "/continuous-typography/static/css/main.0e152822.chunk.css"
+  },
+  {
+    "revision": "ad003b0d614e6009a6f0",
+    "url": "/continuous-typography/static/js/2.9bacec31.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/continuous-typography/static/js/2.9bacec31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b87a2580a5e1c8536b",
+    "url": "/continuous-typography/static/js/main.f22d6449.chunk.js"
+  },
+  {
+    "revision": "1ed486970fc6348d2af7",
+    "url": "/continuous-typography/static/js/runtime-main.6c6ad44c.js"
+  }
+]);
